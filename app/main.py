2b0db97d058890c0136116458d43c98cabf51afb
Andrey Korotkiy <Andreikorotkiy@gmail.com>
2023-03-23 15:43:50 +0300

    init structure + ci

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """    
+    return {"message": "Welcome to book recommendation system"}
---
+++ app/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from .main import app
+
+client = TestClient(app)
+
+
+def test_read_main():
+    """Test function"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to book recommendation system"}
