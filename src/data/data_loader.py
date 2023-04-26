e2ab0edabad326abe19b78bcc729a5572141d451
Kirill <kasurkov2797@gmail.com>
2023-04-26 14:18:52 +0400

    Added pandas in poetry.

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -6,14 +6,14 @@ class DataLoader:
     Loads data from specified path
     """
 
-    def load_data(self, path: str) -> pd.DataFrame:
+    def load_data(self, path_to_data: str) -> pd.DataFrame:
         """
         Loads and data and unarchives it if needed
         """
 
-        compression = "gzip" if path.endswith("gz") else None
+        compression = "gzip" if path_to_data.endswith("gz") else None
 
-        df = pd.read_csv(path, compression=compression)
+        df = pd.read_csv(path_to_data, compression=compression)
 
         return df
 
