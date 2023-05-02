f6f0f13524b4f5359ed69a724b247e19f9b886ff
Kirill Surkov <99968223+KASurkov@users.noreply.github.com>
2023-05-02 16:59:09 +0400

    Update data_loader.py
    Black

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -11,8 +11,7 @@ class DataLoader:
         """
         Loads and data and unarchives it if needed
         """
-        
-        COMPRESSION = Literal['gzip', None]
+        COMPRESSION = Literal["gzip", None]
         compression = "gzip" if path_to_data.endswith("gz") else None
         COMPRESSION = compression
 
