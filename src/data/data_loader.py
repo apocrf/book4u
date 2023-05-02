d6ff0d8019a03c4f56e6686b475a83dc04e5e954
Kirill Surkov <99968223+KASurkov@users.noreply.github.com>
2023-05-02 16:56:11 +0400

    Update data_loader.py
    Black cosmetic changes

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from typing import Literal
 
+
 class DataLoader:
     """
     Loads data from specified path
@@ -10,6 +11,7 @@ class DataLoader:
         """
         Loads and data and unarchives it if needed
         """
+        
         COMPRESSION = Literal['gzip', None]
         compression = "gzip" if path_to_data.endswith("gz") else None
         COMPRESSION = compression
