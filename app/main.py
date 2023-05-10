83007b9d83634b454680c88b5cd84cb5f06ebab5
Andrey Korotkiy <Andreikorotkiy@gmail.com>
2023-05-11 01:01:04 +0300

    get recommend endpoint draft

--- app/main.py
+++ app/main.py
@@ -1,13 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class User(BaseModel):
+    tg_id: int
+
+
+class Book(BaseModel):
+    book: str
+    tg_id: int
+
+
 @app.get("/")
-async def root():
+async def root() -> dict:
+    """Root test example
+
+    Returns:
+        dict: hello message
+    """
+    return {"message": "Welcome to book recommendation system"}
+
+
+@app.get("/recommend")
+async def recommend(book_description: Book):
     """_summary_
 
     Returns:
         _type_: _description_
     """
-    return {"message": "Welcome to book recommendation system"}
+
+    return {"message": "recommend"}
