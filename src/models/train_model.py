4c6755449c631b0a65292d40402a06425c5cfe6a
Kirill Voronin <voronin_ka@mail.ru>
2023-05-26 17:35:52 +0300

    Simple model logging into mlflow in docker

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -6,6 +6,14 @@ from sklearn.neighbors import NearestNeighbors  # type: ignore
 
 import joblib  # type: ignore
 
+import mlflow
+import os
+from dotenv import load_dotenv  # type: ignore
+
+load_dotenv()
+remote_server_uri = os.getenv("MLFLOW_TRACKING_URI")
+mlflow.set_tracking_uri(remote_server_uri)
+
 
 @click.command()
 @click.argument("input_path", type=click.Path(exists=True))
@@ -16,7 +24,17 @@ def train_model(input_path: str, output_path: str):
     :param input_path:
     :param output_path:
     """
-    df_vectors = pd.read_parquet(input_path)
-    nn_model = NearestNeighbors()
-    nn_model = nn_model.fit(df_vectors)
-    joblib.dump(nn_model, output_path)
+    with mlflow.start_run():
+        mlflow.get_artifact_uri()
+
+        print(mlflow.get_artifact_uri())
+
+        df_vectors = pd.read_parquet(input_path)
+        nn_model = NearestNeighbors()
+        nn_model = nn_model.fit(df_vectors)
+        joblib.dump(nn_model, output_path)
+        mlflow.sklearn.log_model(
+            sk_model=nn_model,
+            artifact_path="model",
+            registered_model_name="real_estate_nn",
+        )
